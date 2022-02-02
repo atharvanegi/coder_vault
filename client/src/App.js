96db73da556c9da38221f5deb6e6cf23e6462cc6
unknown <atharvanegi232@gmail.com>
2022-02-02 17:22:05 +0530

    message

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,11 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
+import Dashboard from './components/auth/dashboard/Dashboard';
+
+import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile  from './components/profile-forms/CreateProfile';
+import EditProfile  from './components/profile-forms/EditProfile';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -37,6 +42,9 @@ const App=()=>{
     <Switch>
         <Route exact path='/register' component={Register} />
         <Route exact path='/login' component={Login} />
+        <PrivateRoute exact path='/dashboard' component={Dashboard} />
+        <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+        <PrivateRoute exact path='/edit-profile' component={EditProfile} />
     </Switch>
     </section>
    
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,14 +3,21 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { logout } from "../../actions/auth";
-export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <a onClick={logout} href="#!"><i className="fas.fa-sign-out-alt" ></i>{' '}
-        <span className="hide-sm">Logout</span></a>
+        <Link to="/dashboard">
+          <i className="fas fa-user"></i>{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
       </li>
-      
+      <li>
+        <a onClick={logout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
     </ul>
   );
   const guestLinks = (
@@ -34,17 +41,18 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
           <i className="fab fa-vimeo-v"></i> Coder's Vault
         </Link>
       </h1>
-      {!loading && (<Fragment> { isAuthenticated ? authLinks : guestLinks}</Fragment>) }
-     
+      {!loading && (
+        <Fragment> {isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
 Navbar.propTypes = {
   logout: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
 };
-const mapStateToProps = state => ({
-  auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, { logout })(Navbar);
